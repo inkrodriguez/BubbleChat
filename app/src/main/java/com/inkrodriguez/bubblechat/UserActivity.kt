c2a6baf3fc5f7ddd46df232c29f71b3e7a84864b
Rodrigo <rodrigorasilva@hotmail.com>
2023-03-15 17:07:44 -0300

    adicionada função de enviar mensagem, registrando com o id do documento em sequência, seguindo a ordem de cadastro, porém com bug de registros em looping.

--- app/src/main/java/com/inkrodriguez/bubblechat/UserActivity.kt
+++ app/src/main/java/com/inkrodriguez/bubblechat/UserActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.tasks.Tasks.await
 import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.Query
@@ -14,6 +15,7 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.inkrodriguez.bubblechat.data.*
 import com.inkrodriguez.bubblechat.databinding.ActivityUserBinding
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 
@@ -39,12 +41,16 @@ class UserActivity : AppCompatActivity() {
 
         binding.btnEnviarMessage.setOnClickListener {
             getList()
-            recoverId {
-                var docId = it?.substringBefore(",")
-                var idChat = it?.substringAfter(",")
-                Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
-                binding.editMessage.setText("$docId e $idChat")
-            }
+            enviarMensagem()
+//            recoverId {
+//                var docId = it?.substringBefore(",").toString().toInt()
+//                var idChat = it?.substringAfter(",").toString().toInt()
+//
+//                var novoDocId = docId.plus(1)
+//                var novoIdChat = idChat.plus(1)
+//                Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+//                binding.editMessage.setText("$novoDocId e $novoIdChat")
+//            }
         }
     }
 
@@ -53,7 +59,7 @@ class UserActivity : AppCompatActivity() {
         lifecycleScope.launch {
             var lista: MutableList<Chat> = mutableListOf()
 
-            db.collection("messages").orderBy(FieldPath.documentId(), Query.Direction.ASCENDING).addSnapshotListener { value, error ->
+            db.collection("messages").orderBy("id", Query.Direction.ASCENDING).addSnapshotListener { value, error ->
                 value?.documents?.forEach {
 
                     var recyclerView = binding.recyclerViewChat
@@ -80,70 +86,35 @@ class UserActivity : AppCompatActivity() {
                     return@EventListener
                 }
 
-                for (doc in value!!) {
-                    lastId = doc.get("id").toString()
-                    lastFieldPath = doc.reference.id
+                value?.forEach {
+                    lastId = it.get("id").toString()
+                    lastFieldPath = it.reference.id
                 }
 
                 callback("$lastId,$lastFieldPath")
             })
     }
 
-
-
-
     fun enviarMensagem() {
+        getList()
+        recoverId {
+            var docId = it?.substringBefore(",").toString().toInt()
+            var idChat = it?.substringAfter(",").toString().toInt()
+
+            var novoDocId = docId.plus(1)
+            var novoIdChat = idChat.plus(1)
         lifecycleScope.launch {
-            val chatCollection = db.collection("messages")
+            val chatCollection = db.collection("messages").document("$novoDocId")
             var message = binding.editMessage.text.toString()
 
             var messageMap = hashMapOf(
+                "id" to novoIdChat,
                 "message" to message
             )
 
-            val newDoc = chatCollection.add(messageMap)
-
-            newDoc.addOnCompleteListener {
-                Log.d(TAG, it.result.id)
-            }
-
-//            newDoc.addOnSuccessListener {
-//                //ID DO FIELDPATCH
-//                var getFieldPathId = it.id
-//
-//                var getDocId = it.get().result.getString("id")
-//
-//                Toast.makeText(this@UserActivity, "$getFieldPathId e $getDocId", Toast.LENGTH_SHORT).show()
-//
-//            }
-
-
-
-//            newDoc.addOnSuccessListener {
-//                val documentId = it.id + 1
-//                Toast.makeText(this@UserActivity, documentId, Toast.LENGTH_SHORT).show()
-//                chatCollection.document(documentId).update(FieldPath.documentId(), documentId).addOnCompleteListener {
-//                    getList()
-//                    Toast.makeText(this@UserActivity, "os Dados foram atualizados", Toast.LENGTH_SHORT).show()
-//                }
-//
-//            }
-
-//            //Adiciona um novo documento
-//            newDoc.addOnCompleteListener {
-//                //colocar um som ao enviar a mensagem
-//                Toast.makeText(this@UserActivity, "tudo certo $it", Toast.LENGTH_SHORT).show()
-//            }.addOnFailureListener {
-//                Toast.makeText(this@UserActivity, "deu erro", Toast.LENGTH_SHORT).show()
-//            }
-
-//
-//                db.collection("messages").document("3").set(messageMap).addOnCompleteListener {
-//                    if (it.isSuccessful) {
-//                        Toast.makeText(this@UserActivity, "tudo ok", Toast.LENGTH_SHORT).show()
-//                    }
-//                }.addOnFailureListener {
-//                }
+            chatCollection.set(messageMap)
+            Toast.makeText(this@UserActivity, novoDocId.toString() , Toast.LENGTH_SHORT).show()
+        }
         }
         }
 }
